1275c0d441c47751c9929e115cf7c6dba3a29520
Yi Xuan Sim <yxuan1996@gmail.com>
2017-10-20 11:33:16 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2) do
     t.string   "last_name"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
-    t.boolean  "active",     default: false
+    t.boolean  "active",     default: false, null: false
   end
 
 end
